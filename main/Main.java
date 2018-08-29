3e01a875542bf5a2e5ad82347fbce22149c8e299
Joel Carneiro <jolasman@hotmail.com>
2018-08-30 00:18:59 +0100

    testing the reading of the string from the user and presenting the number of times each word appears on the sentence

--- main/Main.java
+++ main/Main.java
@@ -5,21 +5,26 @@ import abstract_examples.*;
 public class Main {
 
 	public static void main(String[] args) {
-//		Test test = new Test();
+		Test test = new Test();
 //		test.test_map();
+		test.countWords();
 		
 		
-		Director director = new Director(1, "Manuel Bastos", 1500, "123456789");
-		System.out.println(director.getInfo());
+//		Director director = new Director(1, "Manuel Bastos", 1500, "123456789");
+//		System.out.println(director.getInfo());
+//		
+//		Secretary secretary = new Secretary(2, "Maria Madalena", 1000);
+//		System.out.println(secretary.getInfo());
+//	
+//		Director director1 = new Director(3, "Joaquim da Silva", 3000, "456789123");
+//		System.out.println(director1.getInfo());
+//		
+//		Director director3 = new Director(3, "Jeronimo Santos", 3000, "321654987");
+//		director3.authentication("321654987");
 		
-		Secretary secretary = new Secretary(2, "Maria Madalena", 1000);
-		System.out.println(secretary.getInfo());
-	
-		Director director1 = new Director(3, "Joaquim da Silva", 3000, "456789123");
-		System.out.println(director1.getInfo());
 		
-		Director director3 = new Director(3, "Jeronimo Santos", 3000, "321654987");
-		director3.authentication("321654987");
+		
+		
 
 	}
 	
--- map/Test.java
+++ map/Test.java
@@ -1,22 +1,58 @@
 package map;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Test {
 
 	public Test(){
-		
+
 	}
-	
-	public void test_map(){
+
+
+	public void countWords(){
+
+		System.out.println("Please insert the sentence to analyse. The output will show the number of times each word appears in the sentence.");
+		Scanner scan = new Scanner(System.in);
+		String sentence = scan.nextLine();
+
+		String[] words_array = sentence.split("\\s+");
+
+		int counter;
+		Map<String, Integer> map = new HashMap<>();
+
+		for(String word_in : words_array){
+			
+			if(map.containsKey(word_in)){
+				counter = map.get(word_in);
+				map.put(word_in, counter + 1);
+			}
+			else {
+				map.put(word_in, 1);
+			}
+
+		}
 		
+		for(Map.Entry<String, Integer> entry : map.entrySet()){
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+			
+		}
+		
+		scan.close();
+
+
+	}
+
+
+	public void test_map(){
+
 		String initial_string = "ola eu tenho coisas e eu tenho cenas e coisas .";
 		String[] words = initial_string.split("\\s+");
 		int counter;
 		Map<String, Integer> map = new HashMap<>();
-		
+
 		for(String word_in : words){
-			
+
 			if(map.containsKey(word_in)){
 				counter = map.get(word_in);
 				map.put(word_in, counter + 1);
@@ -25,13 +61,13 @@ public class Test {
 				map.put(word_in, 1);
 			}
 		}	
-		
+
 		for(Map.Entry<String, Integer> entry : map.entrySet()){
 			System.out.println(entry.getKey() + " : " + entry.getValue());  	
 		}
-		
-		
-		
+
+
+
 	}
 
 }
